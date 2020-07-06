6a7c92735a841979f47144dd71528e5047c02489
ph_santosjr <paulo.henrique@databrain.com.br>
2020-07-06 07:24:54 -0300

    fix cron sh

--- start-backup-via-cron.sh
+++ start-backup-via-cron.sh
@@ -2,7 +2,7 @@
 set -e
 echo -e " "
 echo -e " ### Iniciando Backups ### "
-cd /opt/projetos/lojaconectada/docker-db-backup-postgres/
+cd /opt/projetos/docker-db-backup-postgres/
 echo -e " ## Entrou na pasta "
 echo -e " ## Iniciando Bakup LC LOGS "
 docker-compose up --force-recreate --build db-bkp-postgres
